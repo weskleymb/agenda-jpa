ef0993c3cb4200244639ecb51b1338dd897ff85d
Weskley Bezerra <weskleymb@hotmail.com>
2018-09-25 17:20:34 -0300

    commit inicial

--- src/br/senac/rn/agenda/model/Contato.java
+++ src/br/senac/rn/agenda/model/Contato.java
@@ -5,9 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
 import br.senac.rn.agenda.dao.PersistDB;
 
 @Table
@@ -17,8 +15,7 @@ public class Contato implements Serializable, PersistDB, Comparable<Contato> {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_CONTATO")
-	@SequenceGenerator(name="SEQ_CONTATO", sequenceName="id_seq_contato", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String fone;
